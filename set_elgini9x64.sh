d55413efea9bab83d37f43fe2b89689f356633da
elppans <elppansmk@gmail.com>
2024-08-02 00:30:41 -0300

    Update set_elgini9x64.sh

--- set_elgini9x64.sh
+++ set_elgini9x64.sh
@@ -14,7 +14,12 @@ if [ "$busdev" == "20d1:7008" ]; then
         echo -e 'FW_MODELO_IMPRESSORA=1' | sudo tee -a /Zanthus/Zeus/pdvJava/EMUL.INI >> /dev/null
         echo -e 'FW_PORTA_USB' | sudo tee -a /Zanthus/Zeus/pdvJava/EMUL.INI >> /dev/null
     fi
-    sudo tar -zxf elginIZ82_x64.tar.gz -C /Zanthus/Zeus
+    if [ -d /Zanthus/Zeus/lib_u64 ]; then
+    sudo tar -zxf elginIZ82_x64.tar.gz
+    rsync -ahz --info=progress2 lib_u64/ /Zanthus/Zeus/lib_u64/
     sudo chmod -R 777 /Zanthus/Zeus/lib_u64
     sudo ldconfig
+    else
+    echo -e "Diretório \"/Zanthus/Zeus/lib_u64\" não existe!"
+    exit 1
 fi
