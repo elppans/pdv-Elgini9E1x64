0a3f42e3ebec13dc7a610f35e2cb3f8dcedf2e9e
elppans <elppans@gmail.com>
2024-08-02 00:45:45 -0300

    Update config file set_elgini9x64.sh

--- set_elgini9x64.sh
+++ set_elgini9x64.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-source /Zanthus/Zeus/pdvJava/ECFRECEB.CFG
-
+ecfreceb="/Zanthus/Zeus/pdvJava/ECFRECEB.CFG"
+emul="/Zanthus/Zeus/pdvJava/EMUL.INI"
+lib_u64="/Zanthus/Zeus/lib_u64"
 busdev="$(lsusb | grep '20d1:7008' | awk '{print $6}')"
+
+export ecfreceb
+export emul
+export lib_u64
 export busdev
+
+# shellcheck source=/dev/null
+source "$ecfreceb"
 
 if [ "$busdev" == "20d1:7008" ]; then
     echo "Elgin i9"
-    if [ ! "$biblioteca" == "izrcb_R82" ]; then
+    if [ ! "$ecfreceb" == "izrcb_R82" ]; then
+        {
         # echo "!=R82"
-        echo -e 'biblioteca=izrcb_R82\n' | sudo tee /Zanthus/Zeus/pdvJava/ECFRECEB.CFG >> /dev/null
-        echo -e 'FW_FLAGS=2' | sudo tee /Zanthus/Zeus/pdvJava/EMUL.INI >> /dev/null
-        echo -e 'FW_MODELO_IMPRESSORA=1' | sudo tee -a /Zanthus/Zeus/pdvJava/EMUL.INI >> /dev/null
-        echo -e 'FW_PORTA_USB' | sudo tee -a /Zanthus/Zeus/pdvJava/EMUL.INI >> /dev/null
+        echo -e 'biblioteca=izrcb_R82\n'
+        echo -e 'FW_FLAGS=2'
+        echo -e 'FW_MODELO_IMPRESSORA=1'
+        echo -e 'FW_PORTA_USB'
+        } >> "$emul"
     fi
-    if [ -d /Zanthus/Zeus/lib_u64 ]; then
-    sudo tar -zxf elginIZ82_x64.tar.gz
-    rsync -ahz --info=progress2 lib_u64/ /Zanthus/Zeus/lib_u64/
-    sudo chmod -R 777 /Zanthus/Zeus/lib_u64
-    sudo ldconfig
+    if [ -d "$lib_u64" ]; then
+        sudo tar -zxf elginIZ82_x64.tar.gz
+        rsync -ahz --info=progress2 lib_u64/ "$lib_u64"/
+        sudo chmod -R 777 "$lib_u64"
+        sudo ldconfig
     else
-    echo -e "Diretório \"/Zanthus/Zeus/lib_u64\" não existe!"
-    exit 1
+        echo -e "Diretório \"$lib_u64\" não existe!"
+        exit 1
+    fi
 fi
