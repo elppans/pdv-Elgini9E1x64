f3a07bb5972166d2aae5589edf6e010d9666aacc
elppans <elppans@gmail.com>
2024-08-23 02:23:41 -0300

    Update .sh

--- set_Elgini9E1x64.sh
+++ set_Elgini9E1x64.sh
@@ -10,7 +10,7 @@ fi
 ecfreceb="/Zanthus/Zeus/pdvJava/ECFRECEB.CFG"
 emul="/Zanthus/Zeus/pdvJava/EMUL.INI"
 lib_u64="/Zanthus/Zeus/lib_u64"
-pacote="so_u64_E1-01.08.00.tar.gz"
+# pacote="so_u64_E1-01.08.00.tar.gz"
 busdev="$(lsusb | grep '20d1:7008' | awk '{print $6}')"
 
 # Exportar variáveis para o ambiente
@@ -35,7 +35,7 @@ if [ "$busdev" == "20d1:7008" ]; then
         if grep -q "^[^#]*FW_INVERTE_GAVETA" "$emul"; then
             # Adiciona as configurações necessárias com o parâmetro FW_INVERTE_GAVETA
             {
-                echo -e 'FW_FLAGS=2'
+                echo -e 'FW_FLAGS=1'
                 echo -e 'FW_MODELO_IMPRESSORA=0'
                 echo -e 'FW_PORTA_USB'
                 echo -e 'FW_INVERTE_GAVETA'
@@ -48,16 +48,30 @@ if [ "$busdev" == "20d1:7008" ]; then
                 echo -e 'FW_PORTA_USB'
             } >"$emul"
         fi
+    else
+        if grep -q "^[^#]*FW_INVERTE_GAVETA" "$emul"; then
+            # Adiciona as configurações necessárias com o parâmetro FW_INVERTE_GAVETA
+			# cmd
+			sed -i "s/FW_FLAGS=""${FW_FLAGS}""/FW_FLAGS=1/" "$emul"
+			sed -i "s/FW_MODELO_IMPRESSORA=""${FW_MODELO_IMPRESSORA}""/FW_MODELO_IMPRESSORA=0/" "$emul"
+			# cmd
+        else
+            # Adiciona as configurações necessárias
+			# cmd
+			sed -i "s/FW_FLAGS=""${FW_FLAGS}""/FW_FLAGS=2/" "$emul"
+			sed -i "s/FW_MODELO_IMPRESSORA=""${FW_MODELO_IMPRESSORA}""/FW_MODELO_IMPRESSORA=0/" "$emul"
+			# cmd
+        fi
     fi
     # Configura bibliotecas so_u64+E1
-    if [ -d "$lib_u64" ]; then
-        echo "Copiando bibliotecas..."
-        tar -zxf "$pacote"
-        rsync -ahz --info=progress2 so_u64/ "$lib_u64"/
-        chmod -R 777 "$lib_u64"
-        ldconfig
-    else
-        echo -e "Diretório \"$lib_u64\" não existe!"
-        exit 1
-    fi
+    # if [ -d "$lib_u64" ]; then
+    #     echo "Copiando bibliotecas..."
+    #     tar -zxf "$pacote"
+    #     rsync -ahz --info=progress2 so_u64/ "$lib_u64"/
+    #     chmod -R 777 "$lib_u64"
+    #     ldconfig
+    # else
+    #     echo -e "Diretório \"$lib_u64\" não existe!"
+    #     exit 1
+    # fi
 fi
